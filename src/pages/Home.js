c8474c4fff5527041038a190481754884f797f67
Jay WInebrenner <jaywinebrenner@gmail.com>
2024-02-07 19:10:44 -0800

    color

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <section className="home">
             <div className="jake-bg">
-                <div className="logo-wrapper">
+                <div className="container logo-wrapper">
                         <h1>Jake Morris</h1>
                 </div>
                 <img src="/jake3.jpg"></img>
