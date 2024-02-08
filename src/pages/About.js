c4d273da6725f9303ed9ee577014df9d8529087d
Jay WInebrenner <jaywinebrenner@gmail.com>
2024-02-07 16:34:06 -0800

    font

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,11 +9,11 @@ const About = () => {
                 <div className="about__body">
                     <p className="margin-top-zero"><span className="jake">Jake Morris</span> is a drummer, vocalist and multi-instrumentalist living in Portland, OR.</p>
 
-                    <p>Since 2011 he has been the drummer for Stephen Malkmus and The Jicks </p>
+                    <p>Jake began playing drums at the age of 9 and has been a working drummer for over 30 years. Since 2011 he has been the drummer for <a target="_blank" href="http://stephenmalkmus.com/">Stephen Malkmus and The Jicks</a>.</p>
 
-                    <p>Along with Stephen Malkmus and The Jicks, Jake also writes, records and performs with his own instrumental “micro-prog” trio Blesst Chest. </p>
+                    <p>Along with Stephen Malkmus and The Jicks, Jake also writes, records and performs with his own band <a target="_blank" href="https://streetnights.bandcamp.com/music">Street Nights</a> and instrumental “micro-prog” trio <a target="_blank" href="https://blesstchest.bandcamp.com/">Blesst Chest.</a> </p>
 
-                    <p>Jake has toured with various bands since 2001 including: The Joggers, French Kicks, Grails, and The Shaky Hands. He has appeared on The Late Show With David Letterman, Late Night with Jimmy Fallon, and Last Call with Carson Daly.</p>
+                    <p>Jake has toured all over the world with various bands since 2001 including:  <a target="_blank" href="https://open.spotify.com/artist/5gvPjkbnAEFeDYUhHbFqTx">The Joggers</a>,  <a target="_blank" href="https://open.spotify.com/artist/2Jj04JcXjjnE99c4225LHF">French Kicks</a>, <a target="_blank" href="https://grails.bandcamp.com/">Grails</a>, <a target="_blank" href="https://open.spotify.com/artist/1rTxCX0gbH7ULTkSmCYqmx">The Shaky Hands</a> and <a target="_blank" href="https://oneelevenheavy.com/">One Eleven Heavy</a>. He has appeared on The Late Show With David Letterman, Late Night with Jimmy Fallon, and Last Call with Carson Daly.</p>
 
                     <p>Jake proudly plays <a target="_blank" href="https://www.candcdrumsusa.com/artists-li-ri">C and C Custom Drums</a></p>
                 </div>
