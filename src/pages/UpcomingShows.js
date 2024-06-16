2f62022f7214553978353d1c05115402625f5675
Jay Winebrenner <jaywinebrenner@gmail.com>
2024-06-15 20:02:55 -0700

    les sav fav

--- src/pages/UpcomingShows.js
+++ src/pages/UpcomingShows.js
@@ -25,6 +25,16 @@ const UpcomingShows = () => {
                 </div>
 
                 <div class="show">
+                    <h2>Les Savy Fav w/Blesst Chest</h2>
+                    <p>Revolution Hall Friday July 12</p>
+                    <a target="_blank" href="https://www.etix.com/ticket/p/33937445/les-savy-fav-portland-revolution-hall?_gl=1*g4nac0*_gcl_au*NzE5ODE1NTMwLjE3MTg1MDY4OTU.*_ga*MTMzMTA5NTU4My4xNzE4NTA2ODk1*_ga_PQG6FGEXJD*MTcxODUwNjg5NS4xLjEuMTcxODUwNjkxNi4wLjAuMA..&_ga=2.36759973.406015303.1718506896-1331095583.1718506895">Get Tickets</a>
+
+                </div>
+
+                
+
+
+                <div class="show">
                     <h2>Street Nights w/special guest Corrina Repp</h2>
                     <p>Sunday August 4th at Rontoms Sunday Sessions</p>
                     <p>FREE</p>
